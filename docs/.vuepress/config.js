d0d1b0c629fc7df9a2f868b3e53b97affab6a3e2
xiguaxigua <xiguaxigua888@gmail.com>
2019-12-08 14:42:51 +0800

    feat: add auto sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,13 @@
-const sidebar = [
-  '/',
-  '/jiangdoufu'
-]
+const fs = require('fs')
+const path = require('path')
+
+const rootpath = path.resolve(path.dirname(__dirname), './')
+
+const files = fs.readdirSync(rootpath)
+const sidebar = files
+  .filter(item => !['.vuepress', 'README.md'].includes(item))
+  .map(item => `/${item.replace('.md', '')}`)
+sidebar.unshift('/')
 
 module.exports = {
   title: '菜谱',
