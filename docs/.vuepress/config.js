13c2cd72518687a0cef09243b8dbfd4d3323ccc4
xiguaxigua <xiguaxigua888@gmail.com>
2019-12-08 13:11:27 +0800

    feat: change menu

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,21 @@
+const sidebar = [
+  '/',
+  '/jiangdoufu'
+]
+
 module.exports = {
   title: '厨房阿光',
   description: '让你的胃满满登登',
   base: '/cookbook/',
   head: [
     ['link', { rel: 'shortcut icon', href: '/favicon.ico' }]
-  ]
+  ],
+  editLinks: true,
+  editLinkText: '修改此页面',
+  themeConfig: {
+    logo: '/logo.png',
+    sidebar,
+    lastUpdated: '最后更新时间',
+    repo: 'xiguaxigua/cookbook',
+  }
 }
